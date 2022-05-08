e8737e7c59645918721cc3fb6cefbdf92607200d
Ben Hessel <hesselfitness@gmail.com>
2022-05-07 21:02:51 -0400

    added show method to videos controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,6 +3,11 @@ class VideosController < ApplicationController
     def index 
         videos = Video.all
         render json: videos
-    end 
+    end
+
+    def show
+        video = Video.find(params[:id])
+        render json: video
+    end
     
 end
