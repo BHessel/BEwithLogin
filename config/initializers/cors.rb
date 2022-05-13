41f582e6b3f355145b1d0701c182ebfb1f6e0dd9
Ben Hessel <hesselfitness@gmail.com>
2022-05-13 14:53:38 -0400

    cors update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,11 +4,10 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
     
-    # allow do
-    #     # this is where production goes
-    #     # the origin here should be whatever I'm deployed to, e.g. https://blahblah.herokuapp.com
-    #     origin "http://localhost:3000"
-    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
+    allow do
+        # this is where production goes
+        origin "https://netflix-movie-matcher.herokuapp.com/"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
 
 end
