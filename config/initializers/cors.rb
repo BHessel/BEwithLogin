b122c0b54dbc82005c7d175dda8183597d52de6a
Ben Hessel <hesselfitness@gmail.com>
2022-05-14 10:51:27 -0400

    hash out hyphens in Cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -4,11 +4,11 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
     
-    allow do
-        # this is where production goes
-        origins "https://netflix-movie-matcher.herokuapp.com/"
-        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    end
+    # allow do
+    #     # this is where production goes
+    #     origins "https://netflix-movie-matcher.herokuapp.com/"
+    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # end
 
     allow do
         # this is where production goes
