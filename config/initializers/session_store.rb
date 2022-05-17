55fb1c5850c708efd42d9e2bc7b84b8bfec0b992
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 16:36:50 -0400

    session store change, take / off end of production link

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "http://netflixmoviematcher.herokuapp.com/"
+    Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflixmoviematcher.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
 end
