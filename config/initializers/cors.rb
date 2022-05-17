856ff91561a5a03de4a747ca36a8af930af5ad92
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 16:53:16 -0400

    revert

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,14 +1,14 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-        origins "http://localhost:3001, https://netflixmoviematcher.herokuapp.com"
+        origins "http://localhost:3001"
         resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
     
-    # allow do
-    #     # this is where production goes
-    #     origins "https://netflixmoviematcher.herokuapp.com"
-    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-    # end
+    allow do
+        # this is where production goes
+        origins "https://netflixmoviematcher.herokuapp.com"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
 
     # allow do
     #     # this is where production goes
