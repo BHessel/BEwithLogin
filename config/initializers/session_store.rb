3343ae37c06ea215b6282e276fd315cbbe66e706
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 16:24:03 -0400

    rails env

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
-if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_nmm_BE", domain: "*"
+if RAILS_ENV == "production"
+    Rails.application.config.session_store :cookie_store, key: "_nmm_BE", domain: "http://netflixmoviematcher.herokuapp.com/"
 else
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
 end
