d05c446d9535df1aa748f80ebbd9254bd75bbae7
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 16:39:53 -0400

    hyphen add

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflixmoviematcher.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflix-movie-matcher.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
 end
