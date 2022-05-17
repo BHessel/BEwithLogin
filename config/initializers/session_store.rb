c696e4a670d54a8d7e37e8e100baad4294f1e306
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 17:46:02 -0400

    cookie showed on login, did not persist

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,5 @@
-# if Rails.env == "production"
-#     Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflixmoviematcher.herokuapp.com"
-# else
-#     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
-# end
-
-Rails.application.config.session_store :cookie_store, key: '_your_applications_key', same_site: :none, secure: true
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", same_site: :none, secure: true
+else
+    Rails.application.config.session_store :cookie_store, key: "_nmm_BE", same_site: :none, secure: true
+end
