bb671622c4400426279d513ccb70a1d5c0e235b9
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 17:43:15 -0400

    same_site in sesh store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,7 @@
-if Rails.env.production?
-    Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflixmoviematcher.herokuapp.com"
-else
-    Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
-end
+# if Rails.env == "production"
+#     Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflixmoviematcher.herokuapp.com"
+# else
+#     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
+# end
+
+Rails.application.config.session_store :cookie_store, key: '_your_applications_key', same_site: :none, secure: true
