0d43fdbf5c0d1115be2364b8f285d5f64c1480d7
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 17:20:37 -0400

    procfile edit

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,4 @@
 if Rails.env == "production"
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflixmoviematcher.herokuapp.com"
 else
-    Rails.application.config.session_store :cookie_store, key: "_nmm_BE", domain: "https://netflixmoviematcher.herokuapp.com"
-end
+    Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
