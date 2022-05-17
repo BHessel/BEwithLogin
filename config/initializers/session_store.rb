7b02e9c0412ac328e33303b7ed93e59dcdde13b8
Ben Hessel <hesselfitness@gmail.com>
2022-05-17 17:22:51 -0400

    procfile back and sesh store if statement ended

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,3 +2,4 @@ if Rails.env == "production"
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE_Prod", domain: "https://netflixmoviematcher.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
+end
