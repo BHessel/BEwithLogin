7ad0cf1fdd5d5737ea693444167b33196204d26d
Ben Hessel <hesselfitness@gmail.com>
2022-05-23 18:04:23 -0400

    tried changing secure to false

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_nmm_PROD", same_site: :none, secure: true
+    Rails.application.config.session_store :cookie_store, key: "_nmm_PROD", same_site: :none, secure: false
 else
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
 end
