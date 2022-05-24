2daeca6c97273e181686e194f4ca356094cd9bfa
Ben Hessel <hesselfitness@gmail.com>
2022-05-24 13:41:58 -0400

    changed sessions store domain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,9 @@
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_nmm_PROD", same_site: :none, secure: true, domain: :all, tld_length: 2
+    Rails.application.config.session_store :cookie_store,
+    key: "_nmm_PROD",
+    same_site: :none,
+    secure: true,
+    domain: "https://netflixmoviematcher.herokuapp.com"
 else
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
 end
