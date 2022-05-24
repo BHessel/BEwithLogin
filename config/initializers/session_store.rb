8732dd155a739b33764c28ae065c681a02b7f6c0
Ben Hessel <hesselfitness@gmail.com>
2022-05-23 20:32:21 -0400

    moved to app.rb

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -3,7 +3,3 @@ if Rails.env == "production"
 else
     Rails.application.config.session_store :cookie_store, key: "_nmm_BE"
 end
-
-config.middleware.use ActionDispatch::Cookies
-
-config.middleware.use config.session_store, config.session_options
